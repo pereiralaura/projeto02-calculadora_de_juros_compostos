75e3c36fcb593afa40c9c8b32f2cc37b124cc1a4
Laura Pereira <lagpereira28@gmail.com>
2024-07-03 13:39:00 -0300

    feat: calculo de juros compostos com variação mensal e anual

--- main.py
+++ main.py
@@ -1,10 +1,26 @@
 #Fórmula de juros compostos: FV/montante = PV/inicial * (1 + taxa) ** tempo
 
-#descobrindo o montante ao mês
-valor_inicial = float(input("Qual o seu valor inicial?: "))
-taxa = float(input("Qual a taxa de juros mensais?: "))
-tempo = float(input("Qual a ser resgatado? Insira em meses: "))
-taxa_convertida = taxa / 100
-montante = valor_inicial * ((1 + taxa_convertida) ** tempo)
+#inserindo informações
+nome = input("Qual é o seu nome?: ")
+print(f"Olá {nome}, seja muito bem-vindo a calculadora de juros compostos!")
 
-print(f"O seu montante final será de: R$ {montante:.2f}")
+valor_inicial = float(input("Digite a quantidade de dinheiro inicial do investimento: "))
+tempo = int(input("Digite quanto tempo você deixará o dinheiro investido. Digite apenas números: "))
+metodo_tempo = int(input("O período é anual ou mensal? Digite 1 para anual ou 2 para mensal: "))
+taxa = float(input("Digite, em percentual, a taxa de retorno do seu investimento: "))
+metodo_taxa = int(input("A taxa é anual ou mensal? Digite 1 para anual ou 2 para mensal: "))
+
+#conversao
+conversao_taxa = (taxa / 100)
+
+if metodo_tempo != metodo_taxa:
+    
+    if metodo_taxa == 1:
+        conversao_taxa = ((1 + conversao_taxa) ** (1/12)) - 1
+        
+    elif metodo_tempo == 1:
+        tempo = tempo * 12
+
+montante = round(valor_inicial * ((1 + conversao_taxa) ** tempo), 2)
+juros = round((montante - valor_inicial), 2)
+print(f"O seu montante final será de R$ {montante} e você terá ganho R$ {juros}")
